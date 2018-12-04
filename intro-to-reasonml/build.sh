59aee65a994d5ce7d14ade4db3c28a7fdf616ad7
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-12-04 21:25:55 +1000

    fix build script

--- intro-to-reasonml/build.sh
+++ intro-to-reasonml/build.sh
@@ -4,5 +4,5 @@ cd ./intro-to-reasonml
 npm install --production
 cp ./html.js ./node_modules/mdx-deck/lib/html.js
 npm run build
-mv -r ./dist ../out/intro-to-reasonml
+cp -r ./dist ../out/intro-to-reasonml
 cd ../
