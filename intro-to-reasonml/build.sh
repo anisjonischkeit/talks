7aa370476709565804fa27a8df86c4f9de7c716b
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-12-04 21:17:34 +1000

    updated to make builds not install bs-platform

--- intro-to-reasonml/build.sh
+++ intro-to-reasonml/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 cd ./intro-to-reasonml
-npm install
+npm install --production
 cp ./html.js ./node_modules/mdx-deck/lib/html.js
 npm run build
-mv ./dist ../out/intro-to-reasonml
+mv -r ./dist ../out/intro-to-reasonml
 cd ../
